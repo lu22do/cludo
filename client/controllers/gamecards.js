ad83baabd074678faa0ac6bb3df2e0eeee38927e
lu22do <ludovicpierre@yahoo.com>
2017-09-24 14:48:16 -0700

    fixed list of cards

--- client/controllers/gamecards.js
+++ client/controllers/gamecards.js
@@ -7,9 +7,9 @@ Template.gamecards.helpers({
     if (game) {
       return [
         {name: 'Suspect cards', cards: SuspectCards[game.rules]},
-        {name: 'Room cards', cards: SuspectCards[game.rules]},
-        {name: 'Time cards', cards: SuspectCards[game.rules]},
-        {name: 'Weapon cards', cards: SuspectCards[game.rules]}
+        {name: 'Room cards', cards: RoomCards[game.rules]},
+        {name: 'Time cards', cards: TimeCards[game.rules]},
+        {name: 'Weapon cards', cards: WeaponCards[game.rules]}
       ];
     }
   }
